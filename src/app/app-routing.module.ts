3c3f2214e117cfbdc41c768837cb7686f64476fe
mikaela1w <w.mikaela97@gmail.com>
2020-12-07 19:35:17 -0800

    lazy load

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule,Routes } from '@angular/router';
 import { ApiDocumentationComponent } from './api-documentation/api-documentation.component';
 
-const routes: Routes = [{path: 'api-documentation', component:ApiDocumentationComponent}];
+const routes: Routes = [{ path: 'apidoc', loadChildren: () => import('./apidoc/apidoc.module').then(m => m.ApidocModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
