2ec8c89140328ff6c66d7e11ebc026c52fc25681
mikaela1w <w.mikaela97@gmail.com>
2020-12-07 20:34:52 -0800

    testing routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule,Routes } from '@angular/router';
 import { ApiDocumentationComponent } from './api-documentation/api-documentation.component';
+import { MovieDetailsComponent } from './movies/movie-details/movie-details.component';
+import { MovieListComponent } from './movies/movie-list/movie-list.component';
 
-const routes: Routes = [{ path: 'api-documentation', component:ApiDocumentationComponent }];
+const routes: Routes = [
+  {path: 'api-documentation', component:ApiDocumentationComponent},
+  {path: 'nominations', component:MovieListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
